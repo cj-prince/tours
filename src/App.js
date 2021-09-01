0f94b620098aaf8caca622222c0bca877480e101
cj-prince <davids.chijioke@gmail.com>
2021-09-01 22:37:43 +0100

    loading and data fetching

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Tours from './Tours';
+import Loading from './Loading'
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tour = await response.json();
+      setLoading(false)
+      setTours(tour)
+    } catch (error){
+      setLoading(false)
+      console.log(error)
+    }    
+    
+  }
+  useEffect(() => {
+    fetchTours()
+  },[])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <Tours />
+    </main>
   );
 }
 
---
+++ src/Tours.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tours = () => {
+    return (
+        <h2>tours component</h2>
+    )
+}
+
+export default Tours
